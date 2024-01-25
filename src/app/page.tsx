e5851d3a3f5a7471678ed0f3bf6b8e128ee8de97
Mardiansyah17 <mardiansyahm002@gmail.com>
2024-01-25 21:53:19 +0700

    uP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
         <div className={`pb-10`}>
             <Hero/>
 
-            <div className={`grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-10`}>
+            <div className={`grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-6`}>
                 <div
-                    className={` h-60 w-72 sm:w-96 relative rounded-lg overflow-hidden `}>
+                    className={` h-60  relative rounded-lg overflow-hidden `}>
                     <div
                         className={`bg-[url('/img/landing1.png')] absolute top-0 w-full h-full bg-center bg-cover`}>
                         <span className="w-full h-full absolute opacity-65 bg-black"></span>
@@ -24,16 +24,17 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className={`bg-primary-2 h-60 text-white w-72 sm:w-96 rounded-lg relative`}>
-                    <div className={`p-3`}>
-                        <h3>About us</h3>
+                <div className={`bg-primary-2 h-60 text-white  rounded-lg relative lg:flex lg:items-center`}>
+                    <div className={`p-3  lg:basis-4/6   `}>
+                        <h3 className={`lg:text-xl lg:mb-4`}>About us</h3>
                         <p className={`text-sm`}>Gabung bersama kita, mari masuk ke sekolah ini dengan berbagai program
                             program ungulan yang
                             telah kami tawarkan</p>
                     </div>
                     <Image src={'/img/org1.png'} alt={'about'} width={110} height={110}
-                           className={'bottom-0 absolute right-0'}/>
+                           className={'bottom-0 absolute right-0 lg:size-40'}/>
                 </div>
+
             </div>
 
             <Exclusive/>
